397a4159ac3caf918f3fd4a051f0a0524b60c6c1
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-06-28 18:09:38 +0200

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is dedicated to providing high-quality indoor plants
+        to make your home greener and healthier.
+      </p>
+      <p>
+        We believe in sustainability, beauty, and bringing nature closer to you.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
